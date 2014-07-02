22411b5cf0bfe33e4e70a91e8f07eaf73259c3ae
Frédéric Peignot <fpeignot@verimatrix.com>
2014-07-02 15:28:08 +0200

    With a small test suite

---
+++ templates/UnitTest.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# Empty Ruby script
+#
+# This script:
+# * analyses its arguments
+# * Desiplay an onlye help if --help or -h are recognised
+# * Log some information
+# * exit
+#
+# Author::    Frédéric  (mailto:fpeignot@x.y)
+
+require 'minitest/autorun'
+
+class UnitTest < MiniTest::Unit::TestCase
+  def setup
+    @name = "joe"
+  end
+
+  def test_that_kitty_can_eat
+    assert_equal "joe", @name
+  end
+
+  def test_that_it_will_not_blend
+    refute_match /^no/i, @name
+  end
+end
---
+++ templates/all_tests.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+require "./UnitTest.rb"
+require "./meme_tu.rb"
