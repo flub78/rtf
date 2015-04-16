2f5567e7d2f2857d88404b0386632a867b71d94b
Frédéric Peignot <fpeignot@verimatrix.com>
2015-04-16 11:48:06 +0200

    add spec expectations in test_assert

---
+++ rspec_tutorial/user.rb
@@ -0,0 +1,9 @@
+class User
+  def in_role?(role)
+    true
+  end
+
+  def assign_role(role)
+  end
+
+end
--- tests/test_assert.rb
+++ tests/test_assert.rb
@@ -3,7 +3,6 @@
 gem "minitest"
 
 require 'minitest/autorun'
-require 'HexString.rb'
 
 # Simple example of sqlite access
 class TestAssert < MiniTest::Test
@@ -18,4 +17,25 @@ class TestAssert < MiniTest::Test
   def teardown
   end
      
+end
+
+# Use spec to have more descriptive use cases
+# TestAssert is used in this example but it is more common to use the
+# class to test.
+describe TestAssert do
+  before do
+    # @meme = Meme.new
+  end
+
+  describe "when asked about cheeseburgers" do
+    it "must respond positively" do
+      "yes".must_equal "yes"
+    end
+  end
+
+  describe "when asked about blending possibilities" do
+    it "won't say no" do
+      "yes".wont_match /^no/i
+    end
+  end
 end
