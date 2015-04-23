20d711e2189900a9f4a1b5787bba34eecc440732
Frédéric Peignot <fpeignot@verimatrix.com>
2015-04-23 15:38:58 +0200

    Better SQL tests

--- tests/test_dbi_mysql.rb
+++ tests/test_dbi_mysql.rb
@@ -10,21 +10,22 @@ require 'dbi'
 # sudo gem install dbd-sqlite3
 # sudo gem install dbd-mysql
 class TestDBIMysql < MiniTest::Test
-  
+
   # --------------------------------------------------------------------------------
+  # Before each test
   # --------------------------------------------------------------------------------
   def setup
-    
+
     @db_type = "mysql"
     # @db_type = "sqlite"
-    
+
     if (@db_type == "mysql")
       @dbh = DBI.connect('DBI:Mysql:test', 'testuser', 'testpwd')
     else
       @dbh = DBI.connect('DBI:SQLite3:test', 'testuser', 'testpwd')
     end
-      
-    # Create the table    
+
+    # Create the table
     sql = "CREATE TABLE IF NOT EXISTS `simple01` (
       `SongName` varchar(64) NOT NULL,
       `SongLength_s` int(11) NOT NULL
@@ -34,110 +35,114 @@ class TestDBIMysql < MiniTest::Test
       `SongName` varchar(64) NOT NULL,
       `SongLength_s` int(11) NOT NULL
     ) ";
-    
+
     @dbh.do(sql)
-    
-    if (@db_type == "mysql")  
-      row = @dbh.select_one("SELECT VERSION()") 
+
+    if (@db_type == "mysql")
+      row = @dbh.select_one("SELECT VERSION()")
       puts "Server version: " + row[0]
     end
-    
+
   end
 
   # --------------------------------------------------------------------------------
+  # After each test
   # --------------------------------------------------------------------------------
   def teardown
-    
     if (@db_type == "mysql")
       @dbh.disconnect
       assert_equal(@dbh.handle, nil, "Database connection closed")
     end
   end
-  
-  
-  # --------------------------------------------------------------------------------
-  # count the number of result returned by a query
-  # --------------------------------------------------------------------------------
-  def sql_count (dbh, query)
-    sth = dbh.prepare(query)
-    sth.execute
-    count = sth.fetch_all.size
-    sth.finish
-    return count
-  rescue DBI::DatabaseError => e
-    puts "Database error code=#{e.err} #{e.errstr}"
-  end
- 
-  # --------------------------------------------------------------------------------
-  # return the result of a query in a hash
-  # --------------------------------------------------------------------------------
-  def sql_select_one_hash (dbh, query)
-  end
-  
-  # --------------------------------------------------------------------------------
-  # return the result of a query into a hash
-  # --------------------------------------------------------------------------------
-  def sql_select_all_hash (dbh, query)
-    sth = dbh.prepare(query)
-    sth.execute
-    rows = sth.fetch_all
-     
-    names = sth.column_names
 
-    result = []
-    rows.each do |row|    
-      i = 0
-      line = {}
-      names.each do |name|
-        line[name] = row[i]
-        i += 1
-      end
-      result << line
+  # --------------------------------------------------------------------------------
+  # Numeric index of column names
+  # --------------------------------------------------------------------------------
+  def name_index(str_array)
+    res = Hash.new
+    cnt = 0
+    str_array.each do |str|
+      res[str] = cnt
+      cnt += 1
     end
-    
-    sth.finish
-    return result
-  rescue DBI::DatabaseError => e
-    puts "Database error code=#{e.err} #{e.errstr}"
+    return res
   end
 
-# --------------------------------------------------------------------------------
-  # Test basic database accesses  
+  # --------------------------------------------------------------------------------
+  # Test basic database accesses
+  # --------------------------------------------------------------------------------
+  def select_all(dbh, query)
+    # read all
+    puts "###############################################################"
+    begin
+      sth = dbh.execute(query)
+      rows = sth.fetch_all
+      count = rows.count
+      name_index = name_index(sth.column_names)
+      # p "name_intex=" + name_index.to_s
+      0.upto(count-1) do |i|
+        puts "#{i} SongName = \"#{rows[i][name_index['SongName']]}\", SongLength_s = \"#{rows[i][name_index['SongLength_s']]}\""
+      end
+
+      sth.finish
+      puts "###############################################################"
+    rescue DBI::DatabaseError => e
+      puts "Database error code=#{e.err} #{e.errstr}"
+    end
+  end
+
+  # --------------------------------------------------------------------------------
+  # Test basic database accesses
+  # --------------------------------------------------------------------------------
+  def select_one(dbh, query)
+    # read all
+    puts "==============================================================="
+    begin
+      sth = dbh.execute(query)
+      rows = sth.fetch_all
+      count = rows.count
+      
+      if (count > 0)
+        return rows[0]
+      else
+        return nill
+      end
+
+      sth.finish
+      puts "==============================================================="
+    rescue DBI::DatabaseError => e
+      puts "Database error code=#{e.err} #{e.errstr}"
+    end
+  end
+
+  # --------------------------------------------------------------------------------
+  # Test basic database accesses
   # --------------------------------------------------------------------------------
   def test_basic
     assert(@dbh, "Connected to database")
-    
+
     # Insert some rows, use placeholders
     1.upto(13) do |i|
-        sql = "insert into simple01 (SongName, SongLength_s) VALUES (?, ?)"
-        @dbh.do(sql, "Song #{i}", "#{i*10}")
+      sql = "insert into simple01 (SongName, SongLength_s) VALUES (?, ?)"
+      @dbh.do(sql, "Song #{i}", "#{i*10}")
     end
-    
-    # read all  
-    @dbh.select_all('select * from simple01;').each do | row |
-      p row[0]
-    end
-    
+
     # read all
-    sth = @dbh.prepare("select * from simple01;")
-    sth.execute
-    
-    rows = sth.fetch_all do |row|
-      puts "SongName = \"#{row[0]}\", SongLength_s = \"#{row[1]}\""  
+    @dbh.select_all('select * from simple01;').each do | row |
+      # puts "<<<<SongName = \"#{row[0]}\", SongLength_s = \"#{row[1]}\""
     end
-        
-    sth.finish
 
-    p self.sql_select_all_hash(@dbh, 'select * from simple01;')
-     
-    puts "count = #{sql_count(@dbh, 'select * from simple01;')}"
-    puts "count = #{sql_count(@dbh, 'select * from simple02;')}"
+    # Read everything with details
+    select_all(@dbh, 'select * from simple01;')
 
-    # p self.sql_select_one_hash(@dbh, 'select * from simple01 where SongName="Song 7";')
-       
+    # Read one
+    res = select_one(@dbh, 'select * from simple01 where SongName="Song 5";')
+    puts "row = #{res[0]}, #{res[1]}"
+    
+    select_one(@dbh, 'select * from simple02')
+
     # Don't prepare, just do it!
     @dbh.do('delete from simple01')
-
   end
-     
+
 end
